f28c45e26529dcbfaf257e7d43b32f9918e27cb5
CRiPTo BeSReZeN <criptobesrezen.ru>
2017-05-04 21:27:01 +0400

    Tancy s bubenchikom with artists slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 'use strict';
 
 $(document).ready(function () {
+
+    // Косой слайдер
     $('.slider__item').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
-      // autoplay: true,
-      // autoplaySpeed: 5000,
+      autoplay: true,
+      autoplaySpeed: 5000,
       arrows: false,
       infinite: true,
       dots: true,
@@ -13,20 +15,24 @@ $(document).ready(function () {
     });
     $(".gallery__box").lightGallery();
 
-    $('.artists__block--slider').slick({
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       arrows: false,
-       fade: true,
-       asNavFor: '.artists__box--slider'
+    // Слайдер "Artists"
+    $('.artists__slider--top').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        infinite: true,
+        asNavFor: '.artists__slider--bottom'
     });
-
-    $('.artists__box--slider').slick({
-       slidesToShow: 5,
-       slidesToScroll: 1,
-       asNavFor: '.artists__block--slider',
-       arrows: false,
-       dots: false,
-       focusOnSelect: true
-  });
+    $('.artists__slider--bottom').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.artists__slider--top',
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true,
+        infinite: true,
+        prevArrow: "<div class='artists__slider-prev'><i class='fa fa-chevron-circle-left' aria-hidden='true'></i></div>",
+        nextArrow: "<div class='artists__slider-next'><i class='fa fa-chevron-circle-right' aria-hidden='true'></i></div>"
+    });
 });
